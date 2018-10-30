fb2179c9a9bd44060b356d706c5c7df7066ddba1
Gregory Kallai <gerg.kallai@gmail.com>
2018-10-30 10:03:40 +0100

    We added a test to check if ATM has insufficient funds

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ I want to receive a message with my withdrawal details<br>
 As a ATM operator <br>          
 In order ensure that an Acccount holder can only withdraw funds that he has balance for  <br>         
 I want to allow a withdrawal only if there are sufficient funds in the account<br>
+
+As an ATM operator <br>
+In order for our customers to withdraw funds <br>
+I need to make sure that we only allow withdrawals if there are funds available <br>
--- lib/atm.rb
+++ lib/atm.rb
@@ -10,10 +10,13 @@ class Atm
         case
         when insufficient_funds_in_acccount?(amount,account)
             { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            {status: false, message: 'insufficient funds in ATM', date: Date.today}
         else
           perform_transaction(amount,account)
         end
       end
+      
 
       def insufficient_funds_in_acccount?(amount,account)
         amount > account.balance
@@ -23,5 +26,9 @@ class Atm
         @funds -= amount
         account.balance = account.balance - amount
         { status: true, message: 'success', date: Date.today, amount: amount }
+      end
+
+      def insufficient_funds_in_atm?(amount)
+        @funds < amount
       end
 end
