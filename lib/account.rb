398847470f4ee969c0fe6519a0b50af2436248ff
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-30 20:48:26 +0100

    Added account owner and added test to see if the account has an owner

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,28 @@
 class Account
     STANDARD_VALIDITY_YRS = 5
-    attr_accessor :pin_code, :balance, :account_status, :exp_date, :account_name
+    attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
 
-    def initialize()
+    def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         @balance = []
         @account_status = :active
         @exp_date = set_expire_date
-        @account_name = {}
+        @owner = {}
+        set_owner(attrs[:owner])
     end
+
+        def set_owner(obj)
+            obj == nil ? missing_owner : @owner = obj
+        end
+
+        def missing_owner
+            raise "An Account owner is required"
+        end
 
     def set_expire_date
         Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
+
     def deactivate
         @account_status = :disabled
     end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,9 @@
 require './lib/account.rb'
 
 describe Account do
+    let(:person) {instance_double('Person', name: 'Thomas')}
+    subject { described_class.new({owner: person}) }
+
     it 'check length of pin number' do
         expect(subject.pin_code.digits.count).to eq 4
     end
@@ -16,5 +19,9 @@ describe Account do
     
     it 'is expected to disable the account when deactivated' do
         expect(subject.deactivate).to eq :disabled
+    end
+
+    it 'is expected to have an owner' do
+        expect(subject.owner).to eq person
     end
 end
