f2eed40ac7c5678f7e2ab519a538d13aada37fab
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-31 13:24:59 +0100

    created withdraw_funds, which decreases the balance of the account, decreases the funds in the ATM and increases cash in pocket

--- lib/person.rb
+++ lib/person.rb
@@ -32,4 +32,20 @@ class Person
         @account.balance += amount
     end
 
+    def withdraw(args = {})
+        @account == nil ? missing_account : withdraw_funds(args)
+    end
+
+    def withdraw_funds(args)
+        args[:atm] == nil ? missing_atm : atm = args[:atm]
+        account = @account
+        amount = args[:amount]
+        pin = args[:pin]
+        response = atm.withdraw(amount, pin, account)
+        response[:status] == true ? increase_cash(response) : response
+    end
+
+    def increase_cash(response)
+        @cash += response[:amount]
+    end
 end
